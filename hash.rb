75e8ee9229d43046c35b1c107bdc70badff44cdf
Naz Khan <nazmuskhan@yahoo.com>
2017-03-09 07:10:18 +0000

    Done.

--- hash.rb
+++ hash.rb
@@ -1,5 +1,3 @@
-require 'pry';
-
 languages = {
   :oo => {
     :ruby => {
@@ -31,19 +29,3 @@ languages = {
 
   }
 }
-
-def reformat_languages(languages)
-  # your code here
-  new_hash = {}
-  languages.each do |styles, data|
-    data.each do |lang, type|
-      new_hash[lang]
-      type.each do |key, value|
-        new_hash[lang] = {:type => value, :style => []}
-        new_hash[lang][:style] << styles
-      end
-    end
-    new_hash[:javascript][:style] << :oo
-  end
-  new_hash
-end
